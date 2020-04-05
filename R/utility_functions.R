c05e0d2527f78cac3ca258ae3c4000df58d08b19
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2020-04-05 20:54:43 +0100

    first commit

---
+++ R/utility_functions.R
@@ -0,0 +1,135 @@
+loop <- function(..., parallel) {
+  if (parallel) {
+    parallel::parLapply(NULL, ...)
+  } else {
+    lapply(...)
+  }
+}
+
+loop_simplify <- function(..., what) {
+  vapply(loop(...), identity, what)
+}
+
+write_out_rds <- function(dat, my_path, file_name) {
+  
+  dir.create(my_path, FALSE, TRUE)
+  
+  saveRDS(dat, file.path(my_path, file_name))
+  
+}
+
+write_out_csv <- function(dat, my_path, file_name, ...) {
+  
+  dir.create(my_path, FALSE, TRUE)
+  
+  write.csv(dat, 
+            file.path(my_path, file_name),
+            ...)
+  
+}
+
+df_to_list <- function (x, use_names) {
+  keep <- c("names", "class", "row.names")
+  at <- attributes(x)
+  attributes(x) <- at[intersect(names(at), keep)]
+  ret <- unname(lapply(split(x, seq_len(nrow(x))), as.list))
+  if (!use_names) {
+    ret <- lapply(ret, unname)
+  }
+  if (is.character(at$row.names)) {
+    names(ret) <- at$row.names
+  }
+  ret
+}
+
+save_plot <- function(plot_obj, out_pth, out_fl_nm, wdt, hgt){
+  
+  dir.create(out_pth, FALSE, TRUE)
+  png(file.path(out_pth, paste0(out_fl_nm, ".png")),
+      width = wdt,
+      height = hgt,
+      units = "cm",
+      pointsize = 12,
+      res = 300)
+  
+  if(is(plot_obj, "gtable")) {
+    grid::grid.draw(plot_obj)
+  } else {
+    print(plot_obj)
+  }
+  
+  on.exit(dev.off())
+  
+}
+
+write_out_rds <- function(dat, my_path, file_name) {
+  
+  dir.create(my_path, FALSE, TRUE)
+  
+  file_name_2 <- paste0(file_name, ".rds")
+  
+  saveRDS(dat, file.path(my_path, file_name_2))
+  
+}
+
+write_out_csv <- function(dat, my_path, file_name) {
+  
+  dir.create(my_path, FALSE, TRUE)
+  
+  file_name_2 <- paste0(file_name, ".csv")
+  
+  write.csv(
+    dat,
+    file.path(my_path, file_name_2),
+    row.names = FALSE)
+  
+}
+
+copy_from_share <- function(root, fl_path_all) {
+  
+  for (i in seq_along(fl_path_all)){
+    
+    path_i <- fl_path_all[[i]]
+    
+    message(path_i)
+    
+    path_from_i <- path_i
+    
+    # match root
+    path_from_i_no_root <- sub(paste0(root,"/"), "", path_from_i)
+    
+    # match everything before the last occurrence of /
+    path_to_i <- sub("/([^/]*)$", "", path_from_i_no_root)
+    
+    if(!dir.exists(path_to_i)) dir.create(path_to_i, FALSE, TRUE)
+    
+    file.copy(path_from_i, path_to_i, overwrite = FALSE)
+    
+    last_three_digits <- sub("^([^.]*).", "", path_i)
+    
+    if(last_three_digits == "shp"){
+      
+      evr_but_last_three_digits <- sub(".([^.]*)$", "", path_i)
+      
+      path_from_i_1 <- file.path(root, paste0(evr_but_last_three_digits, ".dbf"))
+      file.copy(path_from_i_1, path_to_i, overwrite = FALSE)
+      
+      path_from_i_2 <- file.path(root, paste0(evr_but_last_three_digits, ".prj"))
+      file.copy(path_from_i_2, path_to_i, overwrite = FALSE)
+      
+      path_from_i_3 <- file.path(root, paste0(evr_but_last_three_digits, ".shx"))
+      file.copy(path_from_i_3, path_to_i, overwrite = FALSE)
+      
+    }
+    
+  }
+  
+}
+
+sort_str_as_numbers <- function(char_strings) {
+  
+  numbers <- as.numeric(regmatches(char_strings, regexpr("[0-9]+", char_strings)))
+  
+  char_strings[order(numbers)]
+  
+}
---
+++ play.R
@@ -0,0 +1,102 @@
+
+library(ggplot2)
+library(reshape2)
+
+source(file.path("R", "utility_functions.R"))
+
+
+# load data -------------------------------------------------------------------
+
+
+death <- read.csv(file.path("data", 
+                            "csse_covid_19_time_series", 
+                            "time_series_covid19_deaths_global.csv"))
+
+confirmed <- read.csv(file.path("data", 
+                                "csse_covid_19_time_series", 
+                                "time_series_covid19_confirmed_global.csv"))
+
+recovered <- read.csv(file.path("data", 
+                                "csse_covid_19_time_series",
+                                "time_series_covid19_recovered_global.csv"))
+
+
+# run -------------------------------------------------------------------------
+
+
+country_name <- "Korea, South"
+
+death_sub <- subset(death, Country.Region %in% country_name)
+death_cum <- as.numeric(death_sub[1,5:ncol(death_sub)])
+death_inc <- c(0, diff(death_cum))
+
+conf_sub <- subset(confirmed, Country.Region %in% country_name)
+conf_cum <- as.numeric(conf_sub[1,5:ncol(conf_sub)])
+conf_inc <- c(0, diff(conf_cum))
+
+rec_sub <- subset(recovered, Country.Region %in% country_name)
+rec_cum <- as.numeric(rec_sub[1,5:ncol(rec_sub)])
+rec_inc <- c(0, diff(rec_cum))
+
+df_cum_wide <- data.frame(time = seq_len(length(death_inc)),
+                          date = names(death[5:ncol(death)]),
+                          death = death_cum, 
+                          confirmed = conf_cum, 
+                          recovered = rec_cum)
+
+df_inc_wide <- data.frame(time = seq_len(length(death_inc)),
+                          date = names(death[5:ncol(death)]),
+                          death = death_inc, 
+                          confirmed = conf_inc, 
+                          recovered = rec_inc)
+
+df_cum_wide$date <- sub('.', '', df_cum_wide$date)
+
+df_cum_long <- melt(df_cum_wide, 
+                    id.vars = c("time", "date"),
+                    variable.name = "type")
+
+df_cum_long$date <- paste0(df_cum_long$date, "20")
+df_cum_long$date <- as.Date(df_cum_long$date, '%m.%d.%Y')
+
+df_inc_wide$date <- sub('.', '', df_inc_wide$date)
+
+df_inc_long <- melt(df_inc_wide, 
+                    id.vars = c("time", "date"),
+                    variable.name = "type")
+
+df_inc_long$date <- paste0(df_inc_long$date, "20")
+df_inc_long$date <- as.Date(df_inc_long$date, '%m.%d.%Y')
+
+
+# plotting --------------------------------------------------------------------
+
+
+p_cum <- ggplot(data = df_cum_long, aes(x = date, y = value)) +
+  geom_line() + 
+  geom_point(size = 0.7) +
+  facet_wrap(~ type, ncol = 1, scales = "free_y") +
+  scale_x_date() +
+  scale_y_continuous("Number") +
+  ggtitle("South Korea") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.title.x = element_blank())
+
+p_inc <- ggplot(data = df_inc_long, aes(x = date, y = value)) +
+  geom_line() + 
+  geom_point(size = 0.7) +
+  facet_wrap(~ type, ncol = 1, scales = "free_y") +
+  scale_x_date() +
+  scale_y_continuous("Number") +
+  ggtitle("South Korea") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.title.x = element_blank())
+
+
+# save ------------------------------------------------------------------------
+
+
+save_plot(p_cum, "figures", "cumulative", wdt = 15, hgt = 13)
+save_plot(p_inc, "figures", "daily", wdt = 15, hgt = 13)
